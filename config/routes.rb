3dde39c42cf27b06f00331a8db6e381a3004d4fd
wzdxt <wzdxt0624@gmail.com>
2016-02-05 02:21:14 +0800

    update
    Signed-off-by: wzdxt <wzdxt0624@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'index/:model' => 'resources#index', :defaults => {:format => :xml}
-  get '/' => redirect('index/user')
+  get '/' => redirect('admin')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
