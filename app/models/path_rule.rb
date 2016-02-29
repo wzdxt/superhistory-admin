424f66fd36cc4f3b2b3b4014ac5948d2700d2f6d
戴鑫通 <xintong.dai@wenba100.com>
2016-02-29 16:15:34 +0800

    update
    Signed-off-by: 戴鑫通 <xintong.dai@wenba100.com>

---
+++ app/models/path_rule.rb
@@ -0,0 +1,12 @@
+class PathRule < ActiveRecord::Base
+  establish_connection :rule
+  scope :ord_order, -> { order(:ord) }
+
+  def content_css_paths_data
+    JSON.parse self.content_css_paths
+  end
+
+  def content_css_paths_data=(data)
+    self.content_css_paths = data.to_json
+  end
+end
